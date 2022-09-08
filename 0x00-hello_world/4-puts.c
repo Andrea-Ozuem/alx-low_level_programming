09406a72afde5e7925dd0d6bce6cea59f2969bfc
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-08 14:38:07 +0000

    Fix betty style errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - entry point 
- */
+*main - entry point
+*Return: to 0 to indicate success else false
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
