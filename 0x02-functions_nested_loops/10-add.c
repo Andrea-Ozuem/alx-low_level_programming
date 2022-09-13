845683841bdf4fddae6784ed7ab69be3d8a51d33
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-13 23:16:40 +0000

    ALX

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add - adds
+ *@y: numbers to be addded
+ *@x: numbers to be addded
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
-
 /**
  *print_last_digit - print last digit
  *@n: int
  *Return: last num of int
  */
-
 int print_last_digit(int n)
 {
-	_putchar(n % 10);
-	return (n % 10);
+	int nld;
+	
+	nld = n % 10;
+	if (nld < 0)
+		nld = -1 * nld;
+	_putchar(nld + '0');
+	return (nld);
 }
