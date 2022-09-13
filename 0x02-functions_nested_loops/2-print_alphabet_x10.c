1f219ae37f0e3bab917e7d6c658fead55ba803ba
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-13 18:33:08 +0000

    create files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,20 +3,21 @@
 /**
 *print_alphabet - print alphabet
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	
-	c = 'a';
+
 	i = 0;
 	while (i < 10)
 	{
+		c = 'a';
 		while (c < 123)
 		{
 			_putchar(c);
 			c++;
 		}
-	_putchar(10);
+		_putchar(10);
+		i++;
 	}
 }
