d8817868fa99fb2c1b3fe345109abba41d6b25ea
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-19 17:50:32 +0000

    fix typo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
+i
 /**
 *print_rev - print str in reverse order
 *@s: string
 */
 void print_rev(char *s)
 {
-        int i, j;
+	int i, j;
 
-    i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    for (j = i - 1; j >= 0; j--)
-    {
-        _putchar(s[j]);
-    }
-    _putchar('\n');
+	i = 0;
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+	_putchar(s[j]);
+	}
+	_putchar('\n');
 }
