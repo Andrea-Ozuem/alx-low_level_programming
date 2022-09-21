8f153396ab33d1366b4a59ddc1536045342ac18a
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-21 13:04:48 +0000

    Create string related files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates 2 strings
+ * @dest: dest str
+ * @src: str to be added
+ * Return: pointer to string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k;
+	
+	j = 0;
+	while (dest[j] != '\0')
+		j++;
+	k = 0;
+	while (src[k] != '\0')
+		k++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[j + i] = src[i];
+	dest[j + i] = '\0';
+	return (dest);
+}
