f6890a9314f6c057a46dce51632ea52b204a52a0
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-27 00:23:02 +0100

    Initial commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+  *print_diagsums - print diagonal sum
+  *@a: arr
+  *@size: size of arr
+  */
 void print_diagsums(int *a, int size)
 {
 	int i, j, total1 = 0, total2 = 0;
