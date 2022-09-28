be1dcf86db960ea543e40d318c180113a366bb3b
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-28 20:50:22 +0100

    Fix bug

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,11 @@
   */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
-		_puts_recursion(s);
 	if (*s == '\0')
 		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
