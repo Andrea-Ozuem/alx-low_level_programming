6d0224281b009f6021db6de90ec25406d05b49c9
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-28 17:00:16 +0100

    Create pow recurse

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_pow_recursion - print pow
+*@x: int
+*@y: int
+*Return: int
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	else
+		return (x * _pow(x, y - 1));
+}
