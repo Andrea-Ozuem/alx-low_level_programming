fa4e4fff545a9bf479377d40df910f29b7605c9a
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-28 16:06:17 +0100

    create files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,11 +4,11 @@
   *_puts_recursion - puts a str
   *@s: str
   */
-void _puts_recursion(char *s) 
+void _puts_recursion(char *s)
 {
 	_putchar(*s);
-	s++;    
-        if (*s != '\0')
+	s++;
+	if (*s != '\0')
 		_puts_recursion(s);
 	if (*s == '\0')
 		_putchar('\n');
