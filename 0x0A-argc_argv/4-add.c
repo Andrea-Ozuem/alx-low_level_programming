c0d61b4f07d5af3d9583c28dcb07a364ce6f99ab
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-09-30 14:31:44 +0100

    Fixed error\

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,23 +15,19 @@ int main(int argc, char *argv[])
 	int sum;
 
 	sum = 0;
-	if (argc > 1)
+	if (argc < 1)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (isdigit(*argv[i]))
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", sum);
-	}
-	else
 		printf("%d\n", 0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
