6eecbdd72be37d5d02af9ac05117202fa34a0b6f
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-10-02 21:20:49 +0100

    allow negative value edge case

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,9 +20,11 @@ int main(int argc, char *argv[])
 	}
 
 	cents = atoi(argv[1]);
-	if (cents < 0)
+	if (cents <= 0)
+	{
 		printf("%d\n", 0);
-
+		return (0);
+	}
 	quarters = calculate_quarters(cents);
 	cents = cents - quarters * 25;
 
