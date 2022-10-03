f35f8a2a4b14a5f7d2562742588840323f023999
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-10-03 14:27:37 +0100

    crate strcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,36 @@
-#include::
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *str_concat - concatenates 2 str
+  *@s1: str
+  *@s2: str
+  *Return: pointer to concatenated str
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	char *s;
+
+	s = malloc(sizeof(s1) + sizeof(s2));
+	if (s == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = NULL;
+	i = 0;
+	while (*s1 != '\0')
+	{
+		s[i] = *s1;	
+		s1++;
+		i++;
+	}
+	while (*s2 != '\0')
+	{
+		s[i] = *s2;
+		s2++;
+		i++;
+	}
+	return (s);
+}
