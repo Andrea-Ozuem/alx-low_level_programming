57c4ccf9e6a0d789b6b421e8409dcd0d87acc9d0
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-10-06 21:20:06 +0100

    Update calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,2 +1,25 @@
 #include "main.h"
-#include <stdlib.h.>
+#include <stdlib.h>
+
+/**
+  *_calloc - implementation of calloc using malloc
+  *@nmemb: no of items
+  *@size: size of each item
+  *Return: pointer to arr
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
