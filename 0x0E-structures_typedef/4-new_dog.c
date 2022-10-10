a48f6c7e7b47f1378561bac52dee43b894370092
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-10-10 13:44:08 +0100

    create file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
   *new_dog - creates a new fog
   *@name: name
@@ -10,11 +10,37 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
+	int i, newname, newowner;
 
-	new_dog = malloc(sizeof(dog_t));
+	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == NULL || !name || !owner)
 	{
-		free(new_dog);
+		free(n_dog);
 		return (NULL);
 	}
+	for (newname = 0; name[newname]; newname++)
+		;
+	for (newowner = 0; owner[newowner]; newowner++)
+		;
+	
+	n_dog->name = malloc(newname + 1);
+	n_dog->owner = malloc(newowner + 1);
+	if (!n_dog->name || !n_dog->owner)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->age  = age;
 
+	for (i = 0; i < newname;i++)
+		n_dog->name[i] = name[i];
+	n_dog->name[i] = '\0';
+
+	for (i = 0; i < newowner; i++)
+		n_dog->owner[i] = owner[i];
+	n_dog->owner[i] = '\0';
+
+	return (n_dog);
+}
