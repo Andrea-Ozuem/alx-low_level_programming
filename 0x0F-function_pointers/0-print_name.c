0af29c0137070670207944b8c35582e5c7b5b705
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-10-12 11:12:24 +0100

    create print-name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - calls function f for name
+  *@name: name to be printed
+  *@f: funtion  to be called
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
