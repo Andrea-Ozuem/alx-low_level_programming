ec98df17180d372d51b36c1923b0f72a8ab03457
root <root@DESKTOP-32SOABS.localdomain>
2022-10-28 04:59:21 +0100

    fix unkown error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,22 +2,19 @@
 
 /**
   *set_bit - set value of a bit to one at a given index
-  *n: where bits are
+  *@n: where bits are
   *@index: whee bit is licated
   *Return: 1 if successful
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i;
-
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (i = 0; i <= 63; n >>= 1, i++)
+	if (index > 64)
 	{
-		if (index == i)
-			return (n & 1);
+		return (-1);
 	}
-
-	return (-1);
+	else
+	{
+		*n = *n | 1 << index;
+		return (1);
+	}
 }
