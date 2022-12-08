9152f0b9eb99e64a31072ec609f7a8870726a4b0
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-08 21:36:01 +0100

    Add insertion at beginning and end edge case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,11 +12,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *tmp, *new, *prev;
 
+	if (*h == NULL || idx == 0)
+		return (add_dnodeint(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	for (i = 0, tmp = *h; tmp; i++)
-        {
+	{
 		if (i == idx)
 		{
 			prev = tmp->prev;
@@ -28,6 +32,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (new);
 		}
 		tmp = tmp->next;
- 	}
+	}
+	if (idx == i)
+		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
