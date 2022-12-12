943ab39edad5e444d11977ea01dc957de8e70ed4
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-12 22:12:05 +0100

    Add README.MD

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: dest str
+ * @src: str to be added
+ * Return: pointer to string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k;
+
+	j = 0;
+	while (dest[j] != '\0')
+		j++;
+	k = 0;
+	while (src[k] != '\0')
+		k++;
+	for (i = 0; i <= k; i++)
+		dest[j + i] = src[i];
+	dest[j + i] = '\0';
+	return (dest);
+}
